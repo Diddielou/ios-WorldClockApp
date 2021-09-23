a86d42ef368ca7538d5738597ffcf5cd4b13cef7
Nicola Eschbach <nicola.eschbach@hotmail.com>
2021-09-23 15:20:55 +0200

    Initial Commit

---
+++ ios-WorldClockApp/ios_WorldClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_WorldClockAppApp.swift
+//  ios-WorldClockApp
+//
+//  Created by user on 23.09.21.
+//
+
+import SwiftUI
+
+@main
+struct ios_WorldClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
